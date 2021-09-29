3262ac6be4f3a812487903f0d986a7e989a60498
Hans Rietmann <hrietmann@icloud.com>
2021-09-29 13:24:04 +0200

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Hans Rietmann on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Circles
+//
+//  Created by Hans Rietmann on 29/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
